495b08f307dc1fe8460d4cebe3c10dbdd16bd99f
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2019-05-03 11:00:08 -0300

    consertando pokebolas

---
+++ Aula07/src/rs/oisumida/mac0321/ex06/itens/Pokeball.java
@@ -0,0 +1,29 @@
+package rs.oisumida.mac0321.ex06.itens;
+
+import rs.oisumida.mac0321.ex06.Communicator;
+import rs.oisumida.mac0321.ex06.Item;
+import rs.oisumida.mac0321.ex06.Pokemon;
+import rs.oisumida.mac0321.ex06.Trainer;
+
+public class Pokeball extends Item {
+  
+  private final double capture_chance;
+  private final String name;
+
+	public Pokeball(String name, double chance, int max_stack) {
+	  super(name, max_stack, false);
+	  this.name = name;
+		this.capture_chance = chance;
+	}
+	
+	public boolean captureAttempt() {
+	  return true; //FIXME
+	}
+	
+	public boolean applyAs(Trainer player, Pokemon target) {
+		player.givePokemon(target);
+		Communicator.passMessage(player + " capturou " + target);
+		return true;
+	}
+
+}
